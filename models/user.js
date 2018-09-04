6dc02ffb2899600ac80c8999c5a6a912e4d5e381
Neil <neilkosh001@gmail.com>
2018-09-04 19:16:00 -0400

    push all

---
+++ models/user.js
@@ -0,0 +1,28 @@
+var bcrypt = require("bcrypt");
+
+module.exports = (sequelize, DataTypes)=>{
+    let User = sequelize.define("User", {
+        email:{
+            type:DataTypes.STRING,
+            allowNull:false, 
+            unique: true, 
+            validate:{
+                isEmail: true
+            }
+        }, 
+        password:{
+            type: DataTypes.STRING, 
+            allowNull:false
+        }
+    });
+    
+    User.prototype.validPassword = function(password){
+        return bcrypt.compareSync(password,this.password);
+    }
+
+    User.hook("beforeCreate", function(user){
+        user.password= bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+    })
+
+    return User;
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+
+// NPM Packages
+var express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+const db = require("./models");
+// Config
+var passport = require('./helpers/passport');
+var secret = require('./config/keys');
+
+var PORT = process.env.PORT || 8081;
+var mode = process.env.NODE_ENV;
+var app = express();
+
+// MiddleWare
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// Auth & Session Initialization
+app.use(session({ secret: secret.key, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+// Server
+db.sequelize.sync().then(()=>{
+app.listen(PORT, function () {
+    if (mode !== 'production') {
+        var opn = require('opn');
+        opn(`http://localhost:${PORT}`, { app: ['google chrome'] })
+    }
+    console.log(`👋  Hey there I'm 👂 ing on Port: ${PORT}`);
+});
+});
